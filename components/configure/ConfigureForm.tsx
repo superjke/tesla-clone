5c2a6532810769e32276f38a51442706bd850038
Jordan Evans <jordanevans@Jordans-MacBook-Pro.local>
2022-04-23 21:48:17 +0100

    refactor

--- components/configure/ConfigureForm.tsx
+++ components/configure/ConfigureForm.tsx
@@ -4,10 +4,8 @@ import { ISelections } from '../../types/configure'
 import { CONFIG_TYPE, IModel, IModelStats } from '../../types/models'
 import AutoPilotOption from './AutoPilotOption'
 import OptionSelector from './OptionSelector'
+import OrderSummary from './OrderSummary'
 import VideoPreview from './VideoPreview'
-import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew'
-import { Avatar } from '@mui/material'
-import { grey } from '@mui/material/colors'
 
 const getButton = (
   model: string,
@@ -231,108 +229,11 @@ function ConfigureForm(props: IProps) {
           </div>
         </>
       ) : (
-        <>
-          <div className="w-screen px-6 pt-12 pb-24 lg:w-[22rem]">
-            <div className="flex items-center space-x-1">
-              <button onClick={() => toggleSummary()}>
-                <Avatar
-                  sx={{
-                    height: 30,
-                    width: 30,
-                    bgcolor: grey[100],
-                    color: grey[700],
-                  }}
-                >
-                  <ArrowBackIosNewIcon sx={{ fontSize: 14 }} />
-                </Avatar>
-              </button>
-              <span className="text-sm text-gray-500">Edit Design</span>
-            </div>
-            <div className="mt-12  space-y-16 lg:space-y-16">
-              <div>
-                <h1 className="whitespace-nowrap pb-2 text-center text-3xl font-medium">
-                  Your {props.model.name}
-                </h1>
-                <h2 className="whitespace-nowrap text-center text-gray-500">
-                  Est. delivery:{' '}
-                  {props.model.configs
-                    .at(props.selections.config)
-                    ?.estDelivery.toLocaleString('default', {
-                      month: 'long',
-                      year: 'numeric',
-                    })}
-                </h2>
-                <div className="mt-4 w-full lg:px-5">
-                  <ol className="space-y-2 text-center font-light text-gray-600">
-                    <li>
-                      <span>
-                        {props.model.name}{' '}
-                        {props.model.configs.at(props.selections.config)?.name}
-                      </span>
-                    </li>
-                    <li>
-                      <span>
-                        {availableColours.at(props.selections.colour)?.name}{' '}
-                        Paint
-                      </span>
-                    </li>
-                    <li>
-                      <span>
-                        {
-                          props.model.configs
-                            .at(props.selections.config)
-                            ?.wheels.at(props.selections.wheels)?.name
-                        }
-                      </span>
-                    </li>
-                    <li>
-                      <span>
-                        {
-                          props.model.configs
-                            .at(props.selections.config)
-                            ?.interior.at(props.selections.interior)?.name
-                        }{' '}
-                        Interior
-                      </span>
-                    </li>
-                    <li>
-                      <span>Autopilot</span>
-                    </li>
-                    <li>
-                      <span>30-Day Premium Connectivity Trial</span>
-                    </li>
-                  </ol>
-                </div>
-              </div>
-
-              <div>
-                <div className="flex justify-between">
-                  <span>Purchase Price</span>
-                  <span>
-                    £
-                    {props.model.configs
-                      .at(props.selections.config)
-                      ?.price.toLocaleString()}
-                  </span>
-                </div>
-                <div className="mt-3 flex justify-between">
-                  <span>Due today</span>
-                  <span>£200</span>
-                </div>
-                <div className="mt-2">
-                  <span className=" text-sm font-light text-gray-600">
-                    Non-refundable Order Fee*
-                  </span>
-                </div>
-              </div>
-              <div>
-                <button className="w-full rounded-full border-4 border-black py-1">
-                  <span className="text-sm">ORDER WITH CARD</span>
-                </button>
-              </div>
-            </div>
-          </div>
-        </>
+        <OrderSummary
+          model={props.model}
+          selections={props.selections}
+          toggleSummary={toggleSummary}
+        />
       )}
     </div>
   )
