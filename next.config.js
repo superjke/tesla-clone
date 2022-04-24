9b5c3340f735b8d468eb9dce154b16b60bfd631d
Jordan Evans <jordanevans@Jordans-MacBook-Pro.local>
2022-04-24 11:08:20 +0100

    disable swcMinify

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  swcMinify: false,
   images: {
-    domains: ['tesla-cdn.thron.com'],
+    domains: ['tesla-cdn.thron.com', 'static-assets.tesla.com'],
   },
 }
