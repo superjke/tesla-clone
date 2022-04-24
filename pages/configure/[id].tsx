e8f9874914458995b45319f57360e224e2b7776e
Jordan Evans <jordanevans@Jordans-MacBook-Pro.local>
2022-04-24 11:37:47 +0100

    debug

--- pages/configure/[id].tsx
+++ pages/configure/[id].tsx
@@ -32,55 +32,58 @@ function ConfigurePage() {
 
   // TODO: Add loading spinner for preview image
   return (
-    <div className="flex h-screen flex-col lg:overflow-y-hidden">
-      <NavBar />
-
-      {model === undefined ? (
-        <div></div>
-      ) : (
-        <main className="mt-16 lg:flex lg:h-screen lg:flex-col">
-          {previewLoading ? (
-            <div className="-mt-[4rem] flex h-screen items-center justify-center">
-              <CircularProgress />
-            </div>
-          ) : (
-            <></>
-          )}
-          <div className="lg:flex lg:flex-1 lg:overflow-hidden">
-            <div className={previewLoading ? 'hidden' : ''}>
-              <img
-                className="mt-16 h-full w-full object-contain lg:mt-0"
-                src={getPreviewUrl(
-                  model.configs.at(selections.config)?.previewImageUrl,
-                  availableColours.at(selections.colour)?.code,
-                  model.configs
-                    .at(selections.config)
-                    ?.wheels.at(selections.wheels)?.code,
-                  model.configs
-                    .at(selections.config)
-                    ?.interior.at(selections.interior)?.code
-                )}
-                alt=""
-                onLoad={() => setPreviewLoading(false)}
-              />
-            </div>
-            {!previewLoading && (
-              <div className="lg:flex lg:flex-1 lg:flex-col lg:items-center lg:justify-center">
-                <div className="flex flex-1 justify-center lg:w-[500px] lg:overflow-y-auto">
-                  {/* <ConfigureForm
-                    model={model}
-                    selections={selections}
-                    updateSelections={(updatedSelections: ISelections) =>
-                      setSelections(updatedSelections)
-                    }
-                  /> */}
-                </div>
-              </div>
-            )}
-          </div>
-        </main>
-      )}
+    <div className=" flex h-screen w-screen items-center justify-center">
+      <h1>Hello</h1>
     </div>
+    // <div className="flex h-screen flex-col lg:overflow-y-hidden">
+    //   <NavBar />
+
+    //   {model === undefined ? (
+    //     <div><h1>Something went wrong</h1></div>
+    //   ) : (
+    //     <main className="mt-16 lg:flex lg:h-screen lg:flex-col">
+    //       {previewLoading ? (
+    //         <div className="-mt-[4rem] flex h-screen items-center justify-center">
+    //           <CircularProgress />
+    //         </div>
+    //       ) : (
+    //         <></>
+    //       )}
+    //       <div className="lg:flex lg:flex-1 lg:overflow-hidden">
+    //         <div className={previewLoading ? 'hidden' : ''}>
+    //           <img
+    //             className="mt-16 h-full w-full object-contain lg:mt-0"
+    //             src={getPreviewUrl(
+    //               model.configs.at(selections.config)?.previewImageUrl,
+    //               availableColours.at(selections.colour)?.code,
+    //               model.configs
+    //                 .at(selections.config)
+    //                 ?.wheels.at(selections.wheels)?.code,
+    //               model.configs
+    //                 .at(selections.config)
+    //                 ?.interior.at(selections.interior)?.code
+    //             )}
+    //             alt=""
+    //             onLoad={() => setPreviewLoading(false)}
+    //           />
+    //         </div>
+    //         {!previewLoading && (
+    //           <div className="lg:flex lg:flex-1 lg:flex-col lg:items-center lg:justify-center">
+    //             <div className="flex flex-1 justify-center lg:w-[500px] lg:overflow-y-auto">
+    //               <ConfigureForm
+    //                 model={model}
+    //                 selections={selections}
+    //                 updateSelections={(updatedSelections: ISelections) =>
+    //                   setSelections(updatedSelections)
+    //                 }
+    //               />
+    //             </div>
+    //           </div>
+    //         )}
+    //       </div>
+    //     </main>
+    //   )}
+    // </div>
   )
 }
 
