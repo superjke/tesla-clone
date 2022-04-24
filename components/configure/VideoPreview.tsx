5647009558a8bd79e0755900d59e6689173b03cf
Jordan Evans <jordanevans@Jordans-MacBook-Pro.local>
2022-04-24 12:37:07 +0100

    stop autoplay video on mobile

--- components/configure/VideoPreview.tsx
+++ components/configure/VideoPreview.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import useDeviceSize from '../../utils/hooks'
 
 interface IProps {
   url: string
@@ -6,10 +7,17 @@ interface IProps {
 }
 
 function VideoPreview(props: IProps) {
+  const [width, height] = useDeviceSize()
+  const isMobile = width < 768
   return (
     <div className="my-10 flex flex-col space-y-2 text-left">
       <h2 className="pl-6 font-medium">{props.title}</h2>
-      <video className="w-full" autoPlay loop src={props.url}></video>
+      <video
+        className="w-full"
+        autoPlay={isMobile ? true : false}
+        loop
+        src={props.url}
+      ></video>
     </div>
   )
 }
